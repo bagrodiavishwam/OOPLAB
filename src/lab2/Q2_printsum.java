8bfb72e4b35adebaf55c2a58a05703013eeef480
bagrodiavishwam <bagrodiavishwam@gmail.com>
2023-09-02 16:41:05 +0530

    Finished program

--- src/lab2/Q2_printsum.java
+++ src/lab2/Q2_printsum.java
@@ -4,5 +4,16 @@ import java.util.*;
 public class Q2_printsum {
 	public static void main(String [] args) {
 		Scanner sc=new Scanner(System.in);
+		int []a=new int[10];
+		int sum=0;
+		for(int i=0;i<10;i++){
+		System.out.println("Enter value number: "+(i+1));
+		a[i]=sc.nextInt();
+		sum+=a[i];
+		}
+		System.out.println("The sum is: "+sum);
+		sc.close();
+		}
+		}
 	}
 }
