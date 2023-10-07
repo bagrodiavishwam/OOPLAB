a394145622835c26dd6d3f53ca2b197d3e90eec9
bagrodiavishwam <bagrodiavishwam@gmail.com>
2023-10-08 00:15:53 +0530

    created main function

--- src/lab4/Exercise2.java
+++ src/lab4/Exercise2.java
@@ -1,5 +1,30 @@
 package lab4;
 
+import java.io.*;
 public class Exercise2 {
 
+	int [][]arr;
+	int []new_array;
+	
+	void newArray() {
+		int sum=0;
+		int t=0;
+		
+		for(int i = 0;i<arr[0].length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				sum+=arr[j][i];
+			}
+			new_array[t]=sum;
+			++t;
+		}
+	}
+	
+	public static void main(String args[])throws IOException {
+		
+		BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+		
+		
+		
+	}
+	
 }
