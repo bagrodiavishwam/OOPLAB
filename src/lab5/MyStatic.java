98aa2fbb99eee0a28d82993c7259a2ea631c2621
bagrodiavishwam <bagrodiavishwam@gmail.com>
2023-10-08 01:29:01 +0530

    Updated class and method

--- src/lab5/MyStatic.java
+++ src/lab5/MyStatic.java
@@ -8,19 +8,11 @@ public class MyStatic {
 	MyStatic (){ b++; }
 	 public void showData(){
 		System.out.println("Value of a = "+a); 
-	System.out.println("Value of b = "+b); }
-	//public static void increment(){
-	 //a++;
-	 //}
+		System.out.println("Value of b = "+b);
+	}
+	public void increment(){
+	 a++;
+	 }
 }
 
-class StaticDemo{
- public static void main(String args[]){ 
-MyStatic s1 = new MyStatic ();
- s1.showData();
- MyStatic s2 = new MyStatic ();
- s2.showData();
- // MyStatic.b++;
- //s1.showData();
- }
- }
+
---
+++ src/lab5/StaticDemo.java
@@ -0,0 +1,13 @@
+package lab5;
+
+public class StaticDemo {
+
+	 public static void main(String args[]){ 
+		MyStatic s1 = new MyStatic ();
+		 s1.showData();
+		 MyStatic s2 = new MyStatic ();
+		 s2.showData();
+	  MyStatic.b++;
+	 s1.showData();
+	 }
+ }
