3b868ee264f04fb51641cd2bf744bea63f00d4bf
bagrodiavishwam <bagrodiavishwam@gmail.com>
2023-10-08 00:20:51 +0530

    completed implementation

--- src/lab4/Exercise2.java
+++ src/lab4/Exercise2.java
@@ -3,8 +3,8 @@ package lab4;
 import java.io.*;
 public class Exercise2 {
 
-	int [][]arr;
-	int []new_array;
+	static int [][]arr;
+	static int []new_array;
 	
 	void newArray() {
 		int sum=0;
@@ -22,6 +22,24 @@ public class Exercise2 {
 	public static void main(String args[])throws IOException {
 		
 		BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+		
+		System.out.println("Enter the number of rows in 2D Array: ");
+		int r=Integer.parseInt(br.readLine());
+		
+		System.out.println("Enter the number of columns in 2D Array: ");
+		int c=Integer.parseInt(br.readLine());
+		
+		arr=new int[r][c];
+		new_array=new int[r];
+		
+		System.out.println("Enter the 2D array");
+		
+		for(int i=0;i<arr[0].length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				arr[i][j]=Integer.parseInt(br.readLine());
+			}
+			System.out.println();
+		}
 		
 		
 		
