26ae147a99900bc2ecbcb737642bf9c5f213719b
bagrodiavishwam <bagrodiavishwam@gmail.com>
2023-11-28 15:50:19 +0530

    update

--- src/designPatterns/CompositePart.java
+++ src/designPatterns/CompositePart.java
@@ -11,17 +11,32 @@ class Leaf implements Component{
 	
 	int price;
 	String name;
+	
+	public Leaf(int price, String name) {
+		super();
+		this.price = price;
+		this.name = name;
+	}
+
 	@Override
 	public void showPrice() {
 		// TODO Auto-generated method stub
 		System.out.println(name+" : "+price);
 	}
 	
+	
 }
 
 class Composite implements Component{
 	
 	String name;
+	
+	
+	public Composite(String name) {
+		super();
+		this.name = name;
+	}
+
 	List <Component> components = new ArrayList<>();
 	
 	public void addComponent(Component cpm) {
@@ -32,6 +47,9 @@ class Composite implements Component{
 	public void showPrice() {
 		// TODO Auto-generated method stub
 		System.out.println(name);
+	
+		for (Component c : components)
+			c.showPrice();
 	}
 	
 }
--- src/designPatterns/CompositeTest.java
+++ src/designPatterns/CompositeTest.java
@@ -4,7 +4,19 @@ public class CompositeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Component hd = new Leaf(4000, "HDD");
+		Component mouse = new Leaf (400, "Mouse");
+		Component monitor = new Leaf(14000, "Monitor");
+		Component ram = new Leaf (6000,"Ram");
+		Component cpu = new Leaf (40500, "CPU");
+		
+		Composite ph = new Composite("Peri");
+		Composite cabinet = new Composite("Cabinet");
+		Composite mb = new Composite("MB");
+		Composite computer = new Composite("Computer");
+		
+		ph.addComponent(mouse);
+		
 	}
-
 }
+
